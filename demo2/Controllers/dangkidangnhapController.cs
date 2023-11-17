e44a14431d583de5d77d7ae6b7796bb8d7190bdb
quangeagle <haoquang16122004@gmail.com>
2023-11-17 08:48:19 +0700

    demo2

---
+++ demo2/Controllers/dangkidangnhapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo2.Controllers
+{
+    public class dangkidangnhapController : Controller
+    {
+        // GET: dangkidangnhap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ demo2/Controllers/deProController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo2.Controllers
+{
+    public class deProController : Controller
+    {
+        // GET: dePro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
